f89185c49c991f2f2649b5fdf59d0db99ed0b79b
Christopher M. Luciano <cmluciano@us.ibm.com>
2018-06-22 17:52:25 -0400

    Use timeout for reviews service consul
    Signed-off-by: Christopher M. Luciano <cmluciano@us.ibm.com>

--- presentation/manifests/consul-failtest.sh
+++ presentation/manifests/consul-failtest.sh
@@ -17,17 +17,17 @@ echo "Ratings httpFault #####################
 #############################################"
 cat ../scripts/samples/bookinfo/consul/route-rule-ratings-test-2sec-delay.yaml
 
-echo "Ready to create and view 7sec latency to ratings backend"
+echo "Ready to create reviews 1 second timeout"
 read -n 1 -s -r -p "Press any key to continue"
-kubectl apply -f ../scripts/samples/bookinfo/consul/route-rule-ratings-test-delay.yaml 
-echo "Ratings httpFault #####################
+kubectl apply -f ./consul_1secreviews.yaml
+echo "Reviews simpleTimeout #################
 #############################################
 #############################################"
-cat ../scripts/samples/bookinfo/consul/route-rule-ratings-test-delay.yaml 
+cat ./consul_1secreviews.yaml
 
 echo "Ready to cleanup"
 read -n 1 -s -r -p "Press any key to continue"
 kubectl delete -f ../scripts/samples/bookinfo/consul/route-rule-reviews-test-v2.yaml
 kubectl delete -f ../scripts/samples/bookinfo/consul/route-rule-all-v1.yaml
-kubectl delete -f ../scripts/samples/bookinfo/consul/route-rule-ratings-test-delay.yaml 
+kubectl delete -f ../scripts/samples/bookinfo/consul/route-rule-ratings-test-2sec-delay.yaml
 kubectl delete -f ./consul_1secreviews.yaml
